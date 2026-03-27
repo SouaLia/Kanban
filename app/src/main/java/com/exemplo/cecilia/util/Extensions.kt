7effa8803702c4835103627352ead1932d0630c2
20232tiimi0349 <cecilia.tavaresbarreto@gmail.com>
2026-03-27 16:24:55 -0300

    Atividade Kanban - Dispositivos Móveis Continuação do Kanban: 1. Término da Parte 4 (120 a 126) 2. Inicio da Parte 5 (127 a 133)

--- app/src/main/java/com/exemplo/cecilia/util/Extensions.kt
+++ app/src/main/java/com/exemplo/cecilia/util/Extensions.kt
@@ -3,6 +3,10 @@ package com.exemplo.cecilia.task.util
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.exemplo.cecilia.R
+import com.exemplo.cecilia.databinding.BottonSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 fun Fragment.initToolbar(toolbar: Toolbar){
     (activity as AppCompatActivity).setSupportActionBar(toolbar)
     (activity as AppCompatActivity).title=""
@@ -11,3 +15,21 @@ fun Fragment.initToolbar(toolbar: Toolbar){
         activity?.onBackPressedDispatcher?.onBackPressed()
     }
 }
+
+fun Fragment.showBottonSheet(
+    titleDialog: Int? = null,
+    titleButton: Int? = null,
+    mensagem: Int,
+    onClick: () -> Unit ={}
+){/* ERRO 134
+    val bottomSheetDialog = BottomSheetDialog(requireContext())
+    val binding: BottomSheetBinding =
+        BottomSheetBinding.inflate( layoutInflater, null, false)
+    binding.textviewTitle.text= getText(titleDialog ?: R.string.text_title_warning )
+    binding.textviewMessage.text = getText(mensagem)
+    binding.buttonOk.text = getText(titleButton ?: R.string.text_button_warning)
+    binding.buttonOK.setOnClickListener{
+        onClick()
+        bottomSheetDialog.dismiss()
+    }
+}*/
