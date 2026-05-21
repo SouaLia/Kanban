d0511c21a93fa3e65b4ed61f170fbbec4bddee1d
20232TIIMI0349 <cecilia.tavaresbarreto@gmail.com>
2026-05-21 12:06:38 -0300

    Desenvolvimento do Kanban: 1.  Continuação do capíulo 10 (26 a 33) Problemas: as tarefas aparecem errado

--- app/src/main/java/com/exemplo/cecilia/ui/TodoFragment.kt
+++ app/src/main/java/com/exemplo/cecilia/ui/TodoFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.exemplo.cecilia.R
+import com.exemplo.cecilia.data.model.Status
 import com.exemplo.cecilia.data.model.Task
 import com.exemplo.cecilia.databinding.FragmentTodoBinding
 import com.exemplo.cecilia.ui.adapter.TaskAdapter
@@ -89,19 +91,30 @@ class TodoFragment : Fragment() {
                     val taskList = mutableListOf<Task>()
 
                     for (ds in p0.children) {
-                        val task = ds.getValue(Task::class.java)
-                        if (task != null) {
+                        val task = ds.getValue(Task::class.java) as Task
+                    if (task.status == Status.TODO) {
                             taskList.add(task)
                         }
                     }
-
+                    binding.progressBar.isVisible=false
+                    listEmpty(taskList)
                     taskAdapter.submitList(taskList)
+
+                    taskList.reverse()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
                     Toast.makeText(requireContext(), R.string.error_generic, Toast.LENGTH_SHORT).show()
                 }
             })
+    }
+
+    private fun listEmpty(taskList: List<Task>) {
+        binding.textInfo.text = if (taskList.isEmpty()){
+            getString(R.string.text_list_task_empty)
+        } else{
+            ""
+        }
     }
 
     override fun onDestroyView() {
